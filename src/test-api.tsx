4f78a476adda76cdf7e9ece0f9897f868d61f75f
DESKTOP-E6TN2BH\1 <aliaksandr.kazutsin@mbicycle.com>
2021-03-16 22:12:40 +0300

    show list of pokemons on the display

--- src/test-api.tsx
+++ src/test-api.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { InnerContant } from './inner-contant';
+import { Spinner } from './spinner/spinner';
 import './test-api.scss';
 // export interface ArrayData {
 //     id: number,
@@ -7,46 +9,25 @@ import './test-api.scss';
 //     image: string
 // } 
 
-export const TestApi = () => {
+
+export const TestApi: React.FunctionComponent = () => {
     const [getData, setGetData] = useState([]);
     const [loader, setLoader] = useState<boolean>(true);
 
-    const fetchData = async () => {
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`);
-        let result = response.json();
-        if (result) {
-            result.then((data) => {
-                data.results.forEach(pokemon =>
-                    pokemonData(pokemon)
-                );
+    const fetchData = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0`)
+            .then(response => response.json())
+            .then(data => {
+                let results = data.results.map(res => {
+                    return fetch(res.url).then(response => response.json());
+                });
+                return Promise.all(results);
+            }).then((pokeData) => {
+                console.log(pokeData);
+                setGetData(() => pokeData);
             }).catch((err) => {
                 console.error('ERROR', err);
             });
-        }
-    };
-
-    const pokemonData = (pokemon) => {
-        try {
-            const url = pokemon.url;
-            fetch(url)
-                .then(response => response.json())
-                .then((pokeData) => {
-                    const ability = pokeData.types.map((el => el.type.name));
-
-                    const getAllData = {
-                        id: pokeData.id,
-                        name: pokeData.name,
-                        ability: ability,
-                        image: pokeData.sprites.front_shiny
-                    };
-
-                    console.log(getAllData);
-                    setGetData(() => [getAllData]);
-
-                });
-        } catch (error) {
-            console.error('ERROR', error);
-        }
     };
 
     useEffect(() => {
@@ -57,23 +38,16 @@ export const TestApi = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const getPokeInfo = () => {
+        console.log('Info');
+    };
 
     return (
         <>
-            { loader && <span>Loading...</span> }
+            { loader && <Spinner /> }
             {!loader &&
-                <div className='container'>
-                    <ul>
-                        { getData.map((el => {
-                            return <li key={ el.id }>
-                                <img src={ el.image } alt="poke" style={ { width: "150px", height: "150px" } } />
-                                <div>
-                                    <p>{ el.name }</p>
-                                    <p>{ el.ability }</p>
-                                </div>
-                            </li>;
-                        })) }
-                    </ul>
+                <div className='wrapper-items' onClick={ getPokeInfo }>
+                    <InnerContant getData={ getData } />
                 </div> }
         </>
     );
